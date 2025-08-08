fa17a4a17b49254947718e113a65649d25a2be6a
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-08 23:50:04 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i += 2) {
+            answer.add(i);
+        }
+        return answer;
+    }
+}
