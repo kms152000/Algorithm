a102820fe56ed1a316b54cb0b3d82496f3b97e35
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-09 22:26:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int sum = 0;
+        
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += c - '0';
+            }
+        }
+        
+        return sum;
+    }
+}
