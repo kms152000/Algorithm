da90b8e7d1579ec6efe8b014d375111b20b6e75f
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-10 22:43:13 +0900

    [level 0] Title: 약수 구하기, Time: 0.09 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> divisors = new ArrayList<>();
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+                if (i != n / i) divisors.add(n / i);
+            }
+        }
+        Collections.sort(divisors);
+        
+        int[] answer = new int[divisors.size()];
+        
+        for (int i = 0; i < divisors.size(); i++) answer[i] = divisors.get(i);
+        
+        return answer;
+    }
+}
