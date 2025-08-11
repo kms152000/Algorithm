6ddbeed82a34e6c5c8a7a8abe422a35d61cb84a3
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-11 23:33:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = start_num; i >= end_num; i--) {
+            result.add(i);
+        }
+        
+        return result;
+    }
+}
