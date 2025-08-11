adb9cc5c6b82533150ea9213563670072a21316f
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-11 23:31:40 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] num_list, int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = n; i < num_list.length; i++) {
+            result.add(num_list[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            result.add(num_list[i]);
+        }
+        
+        return result;
+    }
+}
