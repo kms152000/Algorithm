90f73993b365d4de1608a8e00902af8212843758
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-13 03:02:14 +0900

    [level 1] Title: K번째수, Time: 1.13 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int idx = 0; idx < commands.length; idx++) {
+            int i = commands[idx][0] - 1;
+            int j = commands[idx][1];
+            int k = commands[idx][2] - 1;
+
+            int[] slice = Arrays.copyOfRange(array, i, j);
+            Arrays.sort(slice);
+            answer[idx] = slice[k];
+        }
+        
+        return answer;
+    }
+}
