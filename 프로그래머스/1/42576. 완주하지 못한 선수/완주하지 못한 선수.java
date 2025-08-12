938d8494f02962b8cafdf47183ed7e5d578c2605
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-13 01:59:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 94.08 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+
+        for (String name : map.keySet()) {
+            if (map.get(name) != 0) {
+                return name;
+            }
+        }
+
+        return ""; // 모든 사람이 완주한 경우 (문제상 그런 경우는 없지만 문법적으로 필요)
+    }
+}
