b5e9341e4936bf56a3a03246651e29aeae5e9de9
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-08-23 20:31:12 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 620 ms, Memory: 142356 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    static void dfs(int v) {
+        visited[v] = true;
+        for (int next : graph[v]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
