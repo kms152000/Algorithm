a428fc0a9505feed7892a773040b5572e7c70c04
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-09-09 23:19:57 +0900

    [Gold III] Title: 줄 세우기, Time: 440 ms, Memory: 44780 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+
+        int[] indeg = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            indeg[b]++;
+        }
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            if (indeg[i] == 0) q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            sb.append(cur).append(' ');
+            for (int nxt : graph[cur]) {
+                if (--indeg[nxt] == 0) q.add(nxt);
+            }
+        }
+
+        bw.write(sb.toString().trim());
+        bw.flush();
+    }
+}
