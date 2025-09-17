c17de38b7b34508f0a8570d90f6b3f94126ad56d
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-09-17 20:55:23 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 136 ms, Memory: 15824 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 1000000;
+
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[N+1][N+1];
+
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = INF;
+            }
+        }
+
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            dist[a][b] = 1;
+            dist[b][a] = 1;
+        }
+
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+
+        int answer = 0;
+        int minSum = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += dist[i][j];
+            }
+            if (sum < minSum) {
+                minSum = sum;
+                answer = i;
+            }
+        }
+
+        
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
