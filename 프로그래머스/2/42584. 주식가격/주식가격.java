bc042f9949dce30e843ca4af6f7491601ee161fb
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-10-12 00:09:23 +0900

    [level 2] Title: 주식가격, Time: 16.60 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int time = 0;
+            for (int j = i + 1; j < n; j++) {
+                time++;
+                if (prices[j] < prices[i]) break;
+            }
+            answer[i] = time;
+        }
+
+        return answer;
+    }
+}
