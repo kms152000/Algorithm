0c55348763c72d7eff7ce7d30dba403cf8cb7fdf
KimMinseok <73588934+kms152000@users.noreply.github.com>
2025-11-09 02:04:13 +0900

    [Gold III] Title: 줄 세우기, Time: 420 ms, Memory: 46084 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,43 +1,50 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
-        int[] indeg = new int[N + 1];
+        List<Integer>[] list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+        int[] degree = new int[N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            indeg[b]++;
+
+            list[a].add(b);
+            degree[b]++;
         }
 
-        ArrayDeque<Integer> q = new ArrayDeque<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
-            if (indeg[i] == 0) q.add(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            sb.append(cur).append(' ');
-            for (int nxt : graph[cur]) {
-                if (--indeg[nxt] == 0) q.add(nxt);
+            if (degree[i] == 0) {
+                queue.offer(i);
             }
         }
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            sb.append(u).append(' ');
+            for (int v : list[u]) if (--degree[v] == 0) queue.offer(v);
+        }
 
-        bw.write(sb.toString().trim());
+        bw.write(String.valueOf(sb));
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
